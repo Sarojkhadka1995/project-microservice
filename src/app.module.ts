624add68f0d3c7af58328c59d27405c1c29c5826
Saroj khadka <saroj.khadka@ekbana.info>
2023-04-11 19:10:24 +0545

    feat: project module with get post endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose/dist/mongoose.module';
+
+//CONTROLLER
 import { AppController } from './app.controller';
+
+//SERVICE
 import { AppService } from './app.service';
+
 import { ProjectModule } from './project/project.module';
 
 @Module({
-  imports: [ProjectModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
+    ProjectModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
